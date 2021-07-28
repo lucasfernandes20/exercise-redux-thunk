c36fe9bcb9e0a3c9d5e5b658376e30a635c42f17
Lucas Fernandes <lucas.h.fernandes.reis@gmail.com>
2021-07-28 18:58:08 -0300

    Realiza exercicio 1

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,6 +5,11 @@ export const requestAPI = () => ({ type: REQUEST_API });
 
 export const getPicture = (data) => ({ type: GET_PICTURE, data });
 
-export function fetchAPI() {
-  // Desenvolva aqui o código da action assíncrona
+export const fetchAPI = () => (dispatch) => {
+  dispatch(requestAPI());
+  return fetch('https://dog.ceo/api/breeds/image/random')
+    .then((r) => r.json()
+      .then(
+        (json) => dispatch(getPicture(json.message))).catch((err) => alert(err))
+    );
 }
